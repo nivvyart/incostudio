6ed0115ac9561713c0baa06ff3e273892bea9284
dazed07 <91119711+dazed07@users.noreply.github.com>
2021-11-21 13:48:29 +1100

    cms base setup

---
+++ content/home.md
@@ -0,0 +1,12 @@
+---
+title: Incostudio 
+date: 2021-03-17T19:31:20.591Z
+people:
+  - description: 'Co-founder and Design Leader of Inco studio, Jayson draws from his diverse background of experience in Europe and Australia. His projects stretch across multiple sectors. Jayson expertly crafts environments that celebrate human interaction and experience.'
+    name: Jayson Lee
+  - description: Co-Founder and Design Strategist of Inco Studio, Bruno is committed to seeking unique and innovative solutions. His specialisation in strategy and design thinking steers the team towards high user-centred quality outcomes. Bruno has vast experience with a variety of positions in leading architectural and design firms.
+    name: Bruno Duval
+---
+Welcome to my awesome page about cats of the internet.
+
+This page is built with NextJS, and content is managed in Netl
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { Component } from 'react'
+import { attributes, react as HomeContent } from '../content/home.md';
 
 export default function Home() {
+  let {title, people} = attributes;
   return (
     <div className="container">
       <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title={title} />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+          <ul>
+            {people.map((person, k) => (
+              <li key={k}>
+                <h2>{person.name}</h2>
+                <p>{person.description}</p>
+              </li>
+            ))}
+          </ul>
+
       </main>
 
       <Footer />
