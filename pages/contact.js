4bb16cca0d614be275bcfc724d90c1a14f66b32e
nivvyart <adrian.thorburn@gmail.com>
2021-11-27 18:54:11 +1100

    form formatting

--- pages/contact.js
+++ pages/contact.js
@@ -9,38 +9,47 @@ const ContactPage = () => {
     <form
       name="contact-form"
       method="POST"
-      action="contact/?success=true"
       data-netlify="true"
+      action="contact/?success=true"
     >
-      <label htmlFor="name">Name *</label>
-      <input 
-        id="name"
-        name="name"
-        required
-        type="text"
-      />
-      <label htmlFor="company"> Company *</label>
-      <input 
-        id="company"
-        name="company"
-        required
-        type="text" />
-      <label htmlFor="email">E-mail Address *</label>
-      <input 
-        id="email"
-        type="email" 
-        name="email" 
-        required 
-      />
-      <label htmlFor="message">Message *</label>
-      <textarea 
-        id="message" 
-        name="message" 
-        required
-      >
-      </textarea>
-      <button type="submit">Submit</button>
-  
+      <div>
+        <label htmlFor="name">Name *</label>
+        <input 
+          id="name"
+          name="name"
+          required
+          type="text"
+        />
+      </div>
+      <div>
+        <label htmlFor="company"> Company *</label>
+        <input 
+          id="company"
+          name="company"
+          required
+          type="text" />
+      </div>
+      <div>
+        <label htmlFor="email">E-mail Address *</label>
+        <input 
+          id="email"
+          type="email" 
+          name="email" 
+          required 
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message *</label>
+        <textarea 
+          id="message" 
+          name="message" 
+          required
+        >
+        </textarea>
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
     </form>
   );
 
