508aef2c4daf999b22865de09c86ab5e6acf739c
nivvyart <adrian.thorburn@gmail.com>
2021-11-28 11:25:12 +1100

    form updates

--- pages/contact.js
+++ pages/contact.js
@@ -1,67 +1,69 @@
-import React from "react";
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+ 
 const ContactPage = () => {
-  const ContactForm = (
-    <form
-      name="contact"
-      method="POST"
-      data-netlify="true"
-      action="contact/?success=true"
-    >
-    <input type="hidden" name="form-name" value="contact" />
-
-      <div>
-        <label htmlFor="name">Name *</label>
-        <input 
-          id="name"
-          name="name"
-          required
-          type="text"
-        />
-      </div>
-      <div>
-        <label htmlFor="company"> Company *</label>
-        <input 
-          id="company"
-          name="company"
-          required
-          type="text" />
-      </div>
-      <div>
-        <label htmlFor="email">E-mail Address *</label>
-        <input 
-          id="email"
-          type="email" 
-          name="email" 
-          required 
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message *</label>
-        <textarea 
-          id="message" 
-          name="message" 
-          required
-        >
-        </textarea>
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  );
-
-  return (
-    <div>
-      <h1>contact us</h1>
-      {ContactForm}
-    </div>
-  )
-
-}
-
+ const [submitterName, setSubmitterName] = useState("");
+ const router = useRouter();
+ const confirmationScreenVisible =
+   router.query?.success && router.query.success === "true";
+ const formVisible = !confirmationScreenVisible;
+ 
+ const ConfirmationMessage = (
+   <React.Fragment>
+     <p>
+       Thank you for submitting this form. Someone should get back to you
+       within 24-48 hours.
+     </p>
+ 
+     <button onClick={() => router.replace("/contact", undefined, { shallow: true })}> Submit Another Response </button>
+   </React.Fragment>
+ );
+ 
+ const ContactForm = (
+   <form
+     className="container"
+     method="POST"
+     name="contact-form"
+     action="contact/?success=true"
+     data-netlify="true"
+     data-netlify-honeypot="bot-field"
+   >
+     <input
+       type="hidden"
+       name="subject"
+       value={`You've got mail from ${submitterName}`}
+     />
+     <input type="hidden" name="form-name" value="contact-form" />
+     <p hidden>
+       <label>
+         Don’t fill this out: <input name="bot-field" />
+       </label>
+     </p>
+ 
+     <label htmlFor="name">Name *</label>
+     <input
+       id="name"
+       name="name"
+       required
+       onChange={(e) => setSubmitterName(e.target.value)}
+       type="text"
+     />
+     <label htmlFor="company">Company *</label>
+     <input id="company" name="company" required type="text" />
+     <label htmlFor="email">E-mail Address *</label>
+     <input id="email" type="email" name="email" required />
+     <label htmlFor="message">Message *</label>
+     <textarea id="message" name="message" required/>
+     <button type="submit">Submit</button>
+   </form>
+ );
+ 
+ return (
+   <div>
+     <h1>Contact Us</h1>
+{formVisible ? ContactForm : ConfirmationMessage}
+   </div>
+ );
+};
+ 
 export default ContactPage;
